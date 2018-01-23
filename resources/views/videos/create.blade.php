a9df16c3217e94ceeceef353e84549f3c322bfd0
Michal31 <michal18.gim@wp.pl>
2018-01-23 18:25:04 +0100

    Stworzenie formularza dodającego nowe video do bazy

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -1,4 +1,19 @@
 @extends('master')
 @section('content')
-<h2>Tutaj bedzie formularz html generujacy film :</h2>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="card">
+            <div class="panel-body">
+            <!-- Formularz -->
+                {!! Form::open(['url'=>'videos','class'=>'form-horizontal']) !!}
+                
+                    @include('videos.form_errors')
+                    @include('videos.form',['buttonText'=>'Dodaj wideo'])
+
+                {!! Form::close() !!}
+                
+            </div>
+        </div>
+    </div>
+</div>
 @stop
